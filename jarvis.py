77390aed4ec96c0319d4ba6c31a14a6200b6cac8
YashTingre <85239029+YashTingre@users.noreply.github.com>
2021-09-03 14:47:34 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -33,6 +33,17 @@ def takeCommand():
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
+       
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
+        print(f"User said: {query}\n")  #User query will be printed.
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")   #Say that again will be printed in case of improper voice 
+        return "None" #None string will be returned
+    return query
 
   
 
